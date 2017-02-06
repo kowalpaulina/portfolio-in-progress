82b337327bfc4b46c33518b5f937c3cf0d1c4834
Paulina Kowal <kowalpaulina@gmail.com>
2017-02-06 14:39:34 +0100

    better formatted photos, new form

--- js/main.js
+++ js/main.js
@@ -8,6 +8,17 @@ $(document).ready(function(){
     overlay();
 });
 
+var heightNav=$('.main-nav').height(); //wys_nav
+var nav=$('nav');
+var navigation=$('.main-nav');
+var icon=$('.mobile-circle-menu i');
+var form = document.querySelector(".form");
+var contactSection = document.querySelector(".contact");
+var box;
+var messageBox;
+var msg;
+var fields = form.querySelectorAll("input, textarea");
+
 $(window).load(function(){
     preloader();
 });
@@ -26,13 +37,10 @@ function move(){
 
 function mark(){
     $(window).scroll(function(){
-        wys_nav=$('.main-nav').height();
-        wys_top=$(window).scrollTop()+wys_nav+5;
+        var topHeight=$(window).scrollTop()+heightNav+5; //wys_top
         $('section').each(function(){
-            nav=$('nav');
-            offset=$(this).offset().top;
             if(nav.hasClass('move'))
-                if(wys_top>=$(this).offset().top){
+                if(topHeight>=$(this).offset().top){
                     $('.btn .move').removeClass('active');
                     $('.btn').removeClass('active');
                     $('#'+$(this).data('type')).addClass('active');
@@ -40,11 +48,90 @@ function mark(){
 
 function scroll(){
     $('.btn').on('click',function(){
-        wys_nav=$('.main-nav').height();
-        button=$(this).attr('id');
-        miejsce=$('.'+button).offset().top;
-        place_nav_wys=miejsce-=wys_nav+1;
-        $('body, html').animate({scrollTop:place_nav_wys+'px'},1200);
+        var button=$(this).attr('id');
+        var btnOffset=$('.'+button).offset().top; //miejsce
+        var placeToScroll=btnOffset-=heightNav+1; //place_nav_wys
+        $('body, html').animate({scrollTop:placeToScroll+'px'},1200);
         return false;});}
 
-function hamburger(){$('.mobile-circle-menu').click(function(){navigation=$('.main-nav');icon=$('.mobile-circle-menu i');navigation.slideToggle(200);if(icon.hasClass('ion-navicon')){icon.addClass('ion-android-close');icon.removeClass('ion-navicon');}else{icon.removeClass('ion-android-close');icon.addClass('ion-navicon');}});}function overlay(){$('.mentors-photo').on('mouseenter touchstartbtap',function(){description=$(this).children().data('desc');title=$(this).children().data('title');$(this).append('<div class="overlay"></div>');overlay=$(this).children('.overlay');overlay.html('<h3>'+title+'</h3><p>'+description+'</p>');overlay.fadeIn(800);preventDefault;});$('.mentors-photo').on('mouseleave vmouseover',function(){overlay.fadeOut(500);});}
+function hamburger(){
+    $('.mobile-circle-menu').click(function(){
+        navigation.slideToggle(200);
+        if(icon.hasClass('ion-navicon')){
+            icon.addClass('ion-android-close');
+            icon.removeClass('ion-navicon');
+        }else{
+            icon.removeClass('ion-android-close');
+            icon.addClass('ion-navicon');}});}
+
+function overlay(){
+    $('.mentors-photo').on('mouseenter touchstartbtap',function(){
+        var description=$(this).children().data('desc');
+        var title=$(this).children().data('title');$(this).append('<div class="overlay"></div>');
+        overlay=$(this).children('.overlay');overlay.html('<h3>'+title+'</h3><p>'+description+'</p>');overlay.fadeIn(800);});$('.mentors-photo').on('mouseleave vmouseover',function(){overlay.fadeOut(500);});}
+
+function showMessage(box, msg) {
+    messageBox = document.querySelector(box);
+    messageBox.innerHTML = msg;
+    messageBox.style.display = 'block';
+}
+
+function sendEmail(e) {
+
+        e.preventDefault();
+        fields = form.querySelectorAll("input, textarea");
+        var data = {};    
+
+    $(".form").serializeArray().map(function(x){data[x.name] = x.value;}); 
+    
+    //doesn't work in IE, Edge
+    //fields.forEach(field => data[field.name] = field.value);
+    
+        $('.error').css("display","none");
+        //document.querySelectorAll(".error").style.display = "none";
+    
+        $.ajax({
+            type:"POST",
+            cache: false,
+            dataType:'json',
+            url:"mail.php",
+            data:data}).done(function(data){ 
+            console.log(data);
+        if(data.error){
+            
+            if(data.error.fnameError){
+                    box = '.error-fname';
+                    msg = data.error.fnameError;
+                    showMessage(box, msg);
+            }
+    
+            if(data.error.emailError){
+                    box = '.error-email';
+                    msg = data.error.emailError;
+                    showMessage(box, msg);
+                };
+    
+            if(data.error.messageError){
+                    box = '.error-message';
+                    msg = data.error.messageError;
+                    showMessage(box, msg);
+                }
+            
+        } else if(data.success){
+            box = '.valid-information';
+            msg = "Dziękuję " + data.dataPerson.fname + ', ' + data.success;
+            showMessage(box, msg);
+            form.classList.add("inActive");
+            contactSection.classList.add("newHight");
+        
+        }
+            
+        else if(data.errorSentMail){
+            box = '.valid-information';
+            msg = data.errorSentMail;
+            showMessage(box, msg);
+        }
+           
+    });
+};
+form.addEventListener("submit", sendEmail, false);
